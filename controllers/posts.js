5d7e9cf6180f0243ce67b44d19e0f3d1a887a308
Smith Scarborough <smithscarborough@gmail.com>
2021-06-18 10:24:23 -0500

    models, routes, controllers, .env

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 // controller: contains the logic, responses for every request to the posts route.
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -11,8 +12,51 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
+
+export const updatePost = async (req, res) => {
+    // receive the ID for the post from 'routes', and rename it to '_id' using object destructuring:
+    const { id: _id } = req.params; 
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that ID.');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatePost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID.');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully.' });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID.');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true }); // note: { new: true } is always required as a second param for findByIdandUpdate
+
+    res.json(updatedPost);
+} 
 
 
--- index.js
+++ index.js
@@ -2,22 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use('/posts', postRoutes);
+dotenv.config();
 
 // limit set since we are going to allow images to be uploaded, which can be large in size
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 
+app.use('/posts', postRoutes);
+
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     // if connection is successful, then app.listen
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(err.message));
